8c7c4aaf2f9367d821074e5ffeee681a76caabb4
aldo larrain <aldo.larrain@hotmail.com>
2020-06-07 19:59:49 -0400

    FUNCIONANDO

---
+++ AppCountry/AppCountry/AppCountry/Services/ApiService.cs
@@ -0,0 +1,38 @@
+﻿namespace AppCountry.Services
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    class ApiService
+    {
+        public async Task<List<T>> Get<T>(
+           string urlBase,
+           string servicePrefix,
+           string controller
+           )
+        {
+            try
+            {
+                var client = new HttpClient();
+                //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                    //tokenType, accessToken);
+                client.BaseAddress = new Uri(urlBase);
+                var url = string.Format("{0}{1}", servicePrefix, controller);
+                var response = await client.GetAsync(url);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+                var result = await response.Content.ReadAsStringAsync();
+                var list = JsonConvert.DeserializeObject<List<T>>(result);
+                return list;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ AppCountry/AppCountry/AppCountry/ViewModels/CountryViewModel.cs
@@ -0,0 +1,59 @@
+﻿using AppCountry.Models;
+using AppCountry.Services;
+
+namespace AppCountry.ViewModels
+{
+    public class CountryViewModel:BaseViewModel
+    {
+        //apiservice
+        #region Services
+        private ApiService apiService;
+        #endregion
+
+        #region Attributes
+        private string name;
+        private string alpha2code;
+        private string capital;
+        #endregion
+        
+        #region Properties
+        public string Name
+        {
+            get { return this.name; }
+            set { SetValue(ref this.name, value); }
+        }
+        public string Capital
+        {
+            get { return this.capital; }
+            set { SetValue(ref this.capital, value); }
+        }
+        public string Alpha2code
+        {
+            get { return this.alpha2code; }
+            set { SetValue(ref this.alpha2code, value); }
+        }
+        #endregion
+        
+        #region Constructor
+        public CountryViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadCountry();
+        }
+        #endregion
+
+        #region Methods
+        private async void LoadCountry()
+        {
+            //llamar API
+            var country = await this.apiService.Get<Country>("https://restcountries.eu/rest/v2/",
+                "name/",
+                "bolivia"
+                );
+            this.Name = country[0].Name;
+            this.Capital = country[0].Capital;
+            this.Alpha2code = country[0].Alpha2Code;
+        }
+        #endregion
+    }
+}
